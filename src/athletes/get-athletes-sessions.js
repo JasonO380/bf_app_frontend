e93f610f3ddebf1a1dbd0278512a910cfa5fccf5
Jason Ollada <JMOllada@gmail.com>
2023-02-09 07:40:58 -0600

    Delete session hooked up

--- src/athletes/get-athletes-sessions.js
+++ src/athletes/get-athletes-sessions.js
@@ -12,16 +12,17 @@ import {
 } from "@chakra-ui/react";
 import { LoginRegisterContext } from "../authentication/login-register-context";
 
+let user
 const GetAthletesSessions = (props) => {
     const newSession = props.newSession;
     const auth = useContext(LoginRegisterContext);
     const [workouts, setWorkouts] = useState([]);
     const getSessions = async () => {
-        const userID = auth.userID;
-        console.log(userID);
+        user = auth.userID;
+        console.log(user);
         try {
             const response = await fetch(
-                `http://localhost:5000/api/users/${userID}`,
+                `http://localhost:5000/api/users/${user}`,
                 {
                     method: "GET",
                     headers: {
@@ -36,22 +37,58 @@ const GetAthletesSessions = (props) => {
         } catch (err) {}
     };
 
+    const deleteSession = async (event) => {
+        console.log(event.target.name)
+        const sessionToDelete = event.target.name;
+        const userID = auth.userID;
+        try {
+            const response = await fetch(
+                `http://localhost:5000/api/users/${userID}`,
+                {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: "Issuer " + auth.token,
+                    },
+                    body: JSON.stringify({
+                        session: sessionToDelete
+                    })
+                }
+            );
+            const responseData = await response.json();
+            console.log(responseData.message);
+        } catch (err) {}
+    }
+
     useEffect(() => {
         getSessions();
         console.log(auth.userID);
-    }, [newSession]);
+    }, [user, newSession]);
 
     return (
         <Stack color="black">
             {workouts.map((s) => {
                 return (
-                    <Text>
-                        Date: {s.date}
-                        Movement: {s.exercise}
-                        Weight: {s.weight}
-                        Reps: {s.reps}
-                        Rounds: {s.rounds}
-                    </Text>
+                    <Box mb={5}>
+                        <Text>
+                            Date: {s.date}
+                            Movement: {s.exercise}
+                            Weight: {s.weight}
+                            Reps: {s.reps}
+                            Rounds: {s.rounds}
+                        </Text>
+                        <Flex mt={2}>
+                            <Button 
+                            bg="teal" 
+                            mr={2}>
+                                Update
+                            </Button>
+                            <Button
+                            name={s._id}
+                            onClick={deleteSession}
+                            bg="red">Delete</Button>
+                        </Flex>
+                    </Box>
                 );
             })}
         </Stack>
