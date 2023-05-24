92ebb4e8ef4a08593d2e312f6ad191ebf15e88c9
Jason Ollada <JMOllada@gmail.com>
2023-05-24 08:17:57 -0500

    Added hamburger menu

---
+++ src/pages/homepage/components/hamburger-menu.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { Box } from "@chakra-ui/react";
+
+const HamburgerMenu = ({ isOpen, toggleMenu }) => {
+    return (
+        <Box
+            as="button"
+            onClick={toggleMenu}
+            pos="relative"
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            width="2.5rem"
+            height="2.5rem"
+            bg="transparent"
+            border="none"
+            cursor="pointer"
+            padding={0}
+            zIndex={1}
+        >
+            <Box
+                w="1.5rem"
+                h="2px"
+                bg="white"
+                borderRadius="2px"
+                transition="all 0.3s ease"
+                transform={
+                    isOpen ? "rotate(-45deg) translate(-4px, 5px)" : "none"
+                }
+            ></Box>
+            <Box
+                w="1.5rem"
+                h="2px"
+                bg="white"
+                borderRadius="2px"
+                opacity={isOpen ? 0 : 1}
+                transition="opacity 0.3s ease"
+                mt="4px"
+            ></Box>
+            <Box
+                w="1.5rem"
+                h="2px"
+                bg="white"
+                borderRadius="2px"
+                transition="all 0.3s ease"
+                transform={
+                    isOpen ? "rotate(45deg) translate(-4px, -5px)" : "none"
+                }
+            ></Box>
+        </Box>
+    );
+};
+
+export default HamburgerMenu;
--- src/pages/mobile-homepage/components/main-nav.js
+++ src/pages/mobile-homepage/components/main-nav.js
@@ -1,17 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import logo from "../../../images/logo.jpeg";
+import HamburgerMenu from "../../homepage/components/hamburger-menu";
 import { NavLink } from "react-router-dom";
 import { Image, Flex, Button, Spacer } from "@chakra-ui/react";
 import { LoginRegisterContext } from "../../../authentication/login-register-context";
+import { motion } from "framer-motion";
 
 const MainNav = () => {
     const auth = useContext(LoginRegisterContext);
     const isLoggedIn = auth.isLoggedIn;
+    const [isMenuOpen, setMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
     const logout = () => {
         auth.logout();
     };
     return (
-        <Flex width="95%" margin="auto">
+        <Flex position="relative" width="95%" margin="auto">
             <Image
                 marginBottom={5}
                 marginTop={5}
@@ -19,6 +25,7 @@ const MainNav = () => {
                 height="2.5rem"
                 src={logo}
             />
+            <HamburgerMenu isOpen={isMenuOpen} toggleMenu={toggleMenu} />
             <Spacer />
             <Flex align="end" margin="auto">
                 {!isLoggedIn ? (
@@ -48,18 +55,18 @@ const MainNav = () => {
                     </>
                 ) : (
                     <>
-                    <NavLink to="/athlete">
+                        <NavLink to="/athlete">
+                            <Button
+                                borderRadius="50"
+                                mt="20px"
+                                fontSize="xs"
+                                color="white"
+                                backgroundColor="transparent"
+                            >
+                                Dashboard
+                            </Button>
+                        </NavLink>
                         <Button
-                            borderRadius="50"
-                            mt="20px"
-                            fontSize="xs"
-                            color="white"
-                            backgroundColor="transparent"
-                        >
-                            Dashboard
-                        </Button>
-                    </NavLink>
-                    <Button
                             borderRadius="50"
                             mt="20px"
                             fontSize="xs"
@@ -69,9 +76,37 @@ const MainNav = () => {
                         >
                             Log Out
                         </Button>
-                        </>
+                    </>
                 )}
             </Flex>
+            {isMenuOpen && (
+                <motion.div
+                    initial={{ opacity: 0, height: 0 }}
+                    animate={{ opacity: 1, height: "auto" }}
+                    exit={{ opacity: 0, height: 0 }}
+                    style={{
+                        position: "absolute",
+                        top: 'calc(100% + 10px)',
+                        left: 0,
+                        right: 0,
+                        borderRadius:"5px",
+                        backgroundColor: "white",
+                        padding: "10px",
+                        zIndex: 2,
+                        width:"fit-content"
+                    }}
+                >
+                    <motion.div>
+                        <NavLink to="/blogs">Blogs</NavLink>
+                    </motion.div>
+                    <motion.div>
+                        <NavLink to="/charts">Charts</NavLink>
+                    </motion.div>
+                    <motion.div>
+                        <NavLink to="/workouts">Workouts</NavLink>
+                    </motion.div>
+                </motion.div>
+            )}
         </Flex>
     );
 };
